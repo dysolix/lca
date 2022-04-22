78f1127b964a9dcad9c40316fc189238b6a77eb1
Felix Matenar <felix.matenar@web.de>
2022-04-22 13:20:37 +0200

    Removed compiled files and scripts

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Data } from "./Data";
-export { default as Client } from "./Client";
-import * as Util_1 from "./Util";
-export { Util_1 as Util };
--- scripts/cleanup.mjs
+++
@@ -1,4 +0,0 @@
-import * as fs from "fs/promises";
-
-await fs.rm("./dist", { recursive: true, force: true});
-await fs.mkdir("./dist");
